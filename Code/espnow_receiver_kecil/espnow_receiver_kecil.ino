c1fb376524959d3b3fce2e699a8a38cf27262b11
ninja1cak <hauzan.khoirullah1022@students.unila.ac.id>
2024-11-18 22:32:01 +0700

    able to display data on lcd

--- Code/espnow_receiver_kecil/espnow_receiver_kecil.ino
+++ Code/espnow_receiver_kecil/espnow_receiver_kecil.ino
@@ -1,3 +1,14 @@
+
+#include "SPI.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
+
+#define TFT_DC D4
+#define TFT_CS D2
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
+
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
@@ -22,19 +33,43 @@ void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
   Serial.print("Bytes received: ");
   Serial.println(len);
   Serial.print("Alat: ");         Serial.println(dataSensor.name);
-  Serial.print("Voltage: ");      Serial.print(dataSensor.voltage);      Serial.println("V");
+  Serial.print("Voltage: ");      Serial.print((isNumber(dataSensor.voltage)) ? dataSensor.voltage : 0);      Serial.println("V");
   Serial.print("Current: ");      Serial.print(dataSensor.current);      Serial.println("A");
   Serial.print("Power: ");        Serial.print(dataSensor.power);        Serial.println("W");
   Serial.print("Energy: ");       Serial.print(dataSensor.energy);     Serial.println("kWh");
   Serial.print("Frequency: ");    Serial.print(dataSensor.frequency); Serial.println("Hz");
   Serial.print("PF: ");           Serial.println(dataSensor.powerFactor);
   Serial.println();
-}
+
+  tft.setCursor(0, 0);
+  measurementText(ILI9341_WHITE, 2, "V: ", "V", 0, 0);
+  measurementText(ILI9341_WHITE, 2, "I: ", "A", 0, 20);
+  measurementText(ILI9341_WHITE, 2, "P: ", "W",0, 40);
+  measurementText(ILI9341_WHITE, 2, "E: ", "kWh",0, 60);
+  measurementText(ILI9341_WHITE, 2, "F: ", "Hz",0, 80);
+
+  tft.setTextSize(2);
+  tft.setCursor(25,0);
+  tft.fillRect(25,20, 20, 20, ILI9341_RED);
+  tft.println((isNumber(dataSensor.voltage)) ? dataSensor.voltage : 0.00000);
+  tft.setCursor(25,20);
+  tft.println((isNumber(dataSensor.current)) ? dataSensor.current : 0.00000);
+  tft.setCursor(25,40);
+  tft.println((isNumber(dataSensor.power)) ? dataSensor.power : 0.00000);
+  tft.setCursor(25,60);
+  tft.println((isNumber(dataSensor.energy)) ? dataSensor.energy : 0.00000);
+  tft.setCursor(25,80);
+  tft.println((isNumber(dataSensor.frequency)) ? dataSensor.frequency : 0.00000);
+
+} 
  
 void setup() {
   // Initialize Serial Monitor
   Serial.begin(115200);
-  
+  tft.begin();
+  tft.setRotation(3);
+  tft.fillScreen(ILI9341_BLACK);
+
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
@@ -50,6 +85,25 @@ void setup() {
   esp_now_register_recv_cb(OnDataRecv);
 }
 
-void loop() {
+void measurementText(int textColor, int textSize, String text, String metric,int pointX, int pointY){
+  tft.setCursor(pointX, pointY);
+  tft.setTextColor(textColor);  
+  tft.setTextSize(textSize);
+  tft.println(text);
   
+  tft.setCursor(100, pointY);
+  tft.println(metric);
+}
+
+bool isNumber(float number){
+  return (number == number) ? true : false;
+}
+
+void loop() {
+
+
+  // tft.setTextColor(ILI9341_WHITE);  tft.setTextSize(2);
+  // tft.println("V : ");
+
+
 }
